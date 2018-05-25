2eea2387ee2d164945ae07da08e375808d375c13
De-Panther <depanther@gmail.com>
2018-05-26 00:26:05 +0300

    Added options to set new Registration Point on play mode: Current main scene = MainNewSequence
    Registration
    T = GetDefault = default value we defined on Israel
    N = NewPrefs = Save new value
    U = DeletePrefs = Delete new value
    R = ResetToTarget
    
    1,2 = move Registration on X
    3,4 = move Registration on Y
    5,6 = move Registration on Z
    
    Directional Light
    Change the Rotation to change the light direction
    
    WorldPoint
    Change the Speed to change the speed of move Registration on X,Y,Z
    
    CameraPointReference
    Position+Rotation relative to the center of the model.
    In Israel we set it to:
    Position = -0.334 -0.01 -1.545
    Rotation = 0 90 0

--- Assets/Scripts/WorldPointController.cs
+++ Assets/Scripts/WorldPointController.cs
@@ -6,17 +6,27 @@ using UnityEngine.XR.WSA.Input;
 public class WorldPointController : MonoBehaviour {
 
 	public Transform cameraTransform;
-	public Vector3 targetPosition;
-	public float targetYRotation;
 
 	public Transform target;
 	public Transform cameraFollow;
 	public Transform worldPointCalc1;
 	public Transform worldPointCalc2;
 
+    public float speed = 0.1f;
+
+    private Vector3 targetDefaultPosition;
+    private Vector3 targetDefaultRotation;
+
 	GestureRecognizer GestureRecognizer;
 
-	void OnEnable()
+    private void Awake()
+    {
+        targetDefaultPosition = target.position;
+        targetDefaultRotation = target.eulerAngles;
+        LoadPrefs();
+    }
+
+    void OnEnable()
 	{
 		GestureRecognizer = new GestureRecognizer();
 		GestureRecognizer.Tapped += GestureRecognizer_Tapped;
@@ -35,21 +45,98 @@ public class WorldPointController : MonoBehaviour {
 
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.R))//(Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            GetDefault();
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            NewPrefs();
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            DeletePrefs();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
 		{
 			ResetToTarget();
 		}
-	}
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            var distance = speed * Time.deltaTime;
+            target.Translate(distance, 0, 0, Space.World);
+            transform.Translate(distance, 0, 0, Space.World);
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            var distance = -speed * Time.deltaTime;
+            target.Translate(distance, 0, 0, Space.World);
+            transform.Translate(distance, 0, 0, Space.World);
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            var distance = speed * Time.deltaTime;
+            target.Translate(0, distance, 0, Space.World);
+            transform.Translate(0, distance, 0, Space.World);
+        }
+        if (Input.GetKey(KeyCode.Alpha4))
+        {
+            var distance = -speed * Time.deltaTime;
+            target.Translate(0, distance, 0, Space.World);
+            transform.Translate(0, distance, 0, Space.World);
+        }
+        if (Input.GetKey(KeyCode.Alpha5))
+        {
+            var distance = speed * Time.deltaTime;
+            target.Translate(0, 0, distance, Space.World);
+            transform.Translate(0, 0, distance, Space.World);
+        }
+        if (Input.GetKey(KeyCode.Alpha6))
+        {
+            var distance = -speed * Time.deltaTime;
+            target.Translate(0, 0, distance, Space.World);
+            transform.Translate(0, 0, distance, Space.World);
+        }
+    }
 
 	private void GestureRecognizer_Tapped(TappedEventArgs args)
 	{
 		//ResetToTarget();
 	}
 
-	public void ResetToTarget()
+    public void GetDefault()
+    {
+        target.position = targetDefaultPosition;
+        target.eulerAngles = targetDefaultRotation;
+    }
+
+    public void NewPrefs()
+    {
+        var posJson = JsonUtility.ToJson(target.position);
+        var rotJson = JsonUtility.ToJson(target.eulerAngles);
+        PlayerPrefs.SetString("targetPosition", posJson);
+        PlayerPrefs.SetString("targetRotation", rotJson);
+    }
+
+    public void LoadPrefs()
+    {
+        if (PlayerPrefs.HasKey("targetPosition") || PlayerPrefs.HasKey("targetRotation"))
+        {
+            var posJson = JsonUtility.ToJson(targetDefaultPosition);
+            var rotJson = JsonUtility.ToJson(targetDefaultRotation);
+            target.position = JsonUtility.FromJson<Vector3>(PlayerPrefs.GetString("targetPosition", posJson));
+            target.eulerAngles = JsonUtility.FromJson<Vector3>(PlayerPrefs.GetString("targetRotation", rotJson));
+        }
+    }
+
+    public void DeletePrefs()
+    {
+        PlayerPrefs.DeleteKey("targetPosition");
+        PlayerPrefs.DeleteKey("targetRotation");
+    }
+
+    public void ResetToTarget()
 	{
-		//transform.localPosition = Vector3.zero - cameraTransform.localPosition + targetPosition;
-		//transform.localRotation = Quaternion.Euler(0f, 0 - cameraTransform.localEulerAngles.y + targetYRotation, 0f);
 		cameraFollow.localPosition = cameraTransform.localPosition;
 		cameraFollow.localEulerAngles = new Vector3(0, cameraTransform.localEulerAngles.y, 0);
 		worldPointCalc1.position = transform.position;
---
+++ readme.txt
@@ -0,0 +1,24 @@
+Current main scene = MainNewSequence
+
+Registration
+T = GetDefault = default value we defined on Israel
+N = NewPrefs = Save new value
+U = DeletePrefs = Delete new value
+R = ResetToTarget
+
+1,2 = move Registration on X
+3,4 = move Registration on Y
+5,6 = move Registration on Z
+
+
+Directional Light
+Change the Rotation to change the light direction
+
+WorldPoint
+Change the Speed to change the speed of move Registration on X,Y,Z
+
+CameraPointReference
+Position+Rotation relative to the center of the model.
+In Israel we set it to:
+Position = -0.334 -0.01 -1.545
+Rotation = 0 90 0
